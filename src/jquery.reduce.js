04b03371e25ca21c60dd981ce3dd6b4082c39cfa
Cesar Kastli <cesarkastli@gmail.com>
2015-10-05 02:33:08 -0300

    CommonJS module aware and loops through object properties, no more compatible with Array.prototype.reduce

--- src/jquery.reduce.js
+++ src/jquery.reduce.js
@@ -1,17 +1,27 @@
-;(function($, undefined) {
+;(function(root, undefined) {
 
-  $.reduce = function(collection, iterator, initial, context) {
-    if (Array.prototype.reduce) {
-      return Array.prototype.reduce.call(collection, iterator.bind(context), initial);
+  'use strict';
+
+  var pluginName = 'reduce';
+
+  var $ = root.jQuery;
+
+  if ('undefined' === typeof $) {
+    if ('undefined' === typeof require) {
+      throw new Error('The plugin "' + pluginName + '" requires jQuery');
     }
+    $ = require('jquery');
+  }
+
+  $[pluginName] = function(collection, iterator, initial, context) {
     $.each(collection, function(index, current) {
       initial = iterator.call(context, initial, current, index);
     });
     return initial;
   };
 
-  $.fn.reduce = function(iterator, initial, context) {
-    return $.reduce(this, iterator, initial, context);
+  $.fn[pluginName] = function(iterator, initial, context) {
+    return $[pluginName](this, iterator, initial, context);
   };
 
-}(jQuery, undefined));
+}(this));
